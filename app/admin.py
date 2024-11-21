2411cec19f3e20dccacbb60f57b3b80b3735e944
Thalison R. <thalison.renan@escolar.ifrn.edu.br>
2024-11-21 07:49:07 -0300

    Aula - 21/11

--- app/admin.py
+++ app/admin.py
@@ -4,8 +4,8 @@ from app.models import Car
 # Register your models here.
 
 class CarFilter(admin.ModelAdmin):
-    list_display = ("id", "brand","model", "year")
-    list_display_links = ("id", "brand","model", "year")
+    list_display = ("id", "user", "brand", "model", "year")
+    list_display_links = ("id", "brand", "model", "year")
     # list_filter = ("brand", "year")  # Caixa de filtros
     search_fields = ("brand", "model")
     
